3857fb933d7c17c55c6674c736e46d8fde555e17
Iulian Dogariu <code@iuliandogariu.com>
2013-11-16 20:21:53 +0200

    hook up to Cron on OS X, and update jobs.

--- lslr-Media.sh
+++ lslr-Media.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+# === On Linux
 # 10 18    * * *   iulian /home/iulian/dotfiles/lslr-Media.sh
 # chmod 644 /etc/cron.d/lslRMedia
 # chown root:root /etc/cron.d/lslRMedia
 # service cron reload
+# === On OS X
+# sudo crontab -u root -e
+# SHELL=/bin/bash
+# MAILTO=iulian
+# PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+# 43 19    * * *  /Users/iulian/dotfiles/lslr-Media.sh 
 
 dates=`date "+%Y%m%d"`
 fname=medialist-$dates
-ls -lR /home/iulian/Music/ > /tmp/$fname
-ls -lR /home/iulian/Store/Movies/ >> /tmp/$fname
-ls -lR /home/iulian/Store/DE_VAZUT/ >> /tmp/$fname
+home=/Users/iulian
+secondaryvol=/Volumes/2tb-p2
+ls -lR $secondaryvol/iTunes\ Media/Music > /tmp/$fname
+ls -lR $secondaryvol/Movies/ >> /tmp/$fname
 gzip -f /tmp/$fname
-mv -f /tmp/$fname.gz /home/iulian/Dropbox/Reference/media/
+mv -f /tmp/$fname.gz $home/Dropbox/Reference/media/
 
--- sync_bitbucket.rb
+++ sync_bitbucket.rb
@@ -1,18 +1,67 @@
+# === On Linux
 # 43 15    * * *   iulian ruby /home/iulian/dotfiles/sync_bitbucket.rb
 # chmod 644 /etc/cron.d/bitbucket
 # chown root:root /etc/cron.d/bitbucket
 # service cron reload
+# === On OS X
+# sudo crontab -u root -e
+# SHELL=/bin/bash
+# MAILTO=iulian
+# PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+# 43 19    * * *   cd /Users/iulian/dotfiles/ && ruby sync_bitbucket.rb
+# ===
+# The file bitbucket_credentials.rb has to conform to this structure:
+#BITBUCKET_USERNAME = "iulianu"
+#BITBUCKET_PASSWORD = "lalala123"
+#LOCAL_DIR = "~/Archive/bitbucket"
+#
 
 require 'net/https'
 require 'yaml'
-require '~/bitbucket_credentials.rb'
+require './bitbucket_credentials'
+require 'pp'
 
 unless (defined?(LOCAL_DIR) && LOCAL_DIR.strip.size > 0)
-  puts "ERROR: add a script called ~/bitbucket_credentials.rb"
+  puts "ERROR: add a script called ~/dotfiles/bitbucket_credentials.rb"
   exit 2
 end
 
 `mkdir -p #{LOCAL_DIR}`
+
+def assert_success(process_status)
+  exit_code = process_status.exitstatus
+  if(exit_code != 0)
+    puts "Last command exited with code #{exit_code}"
+    exit exit_code
+  end
+end
+
+def update_or_clone_hg(slug)
+  repo_url = "https://#{BITBUCKET_USERNAME}:#{BITBUCKET_PASSWORD}@bitbucket.org/#{BITBUCKET_USERNAME}/#{slug}"
+  local_path = "#{LOCAL_DIR}/#{slug}"
+  if File.exists?(File.expand_path("#{LOCAL_DIR}/#{slug}"))
+    puts "Updating #{slug}"
+    `cd #{local_path} && hg pull #{repo_url}`
+  else
+    puts "Cloning #{slug}"
+    `hg clone -U #{repo_url} #{local_path}`
+  end
+  assert_success($?)
+end
+
+def update_or_clone_git(slug)
+  repo_url = "https://#{BITBUCKET_USERNAME}:#{BITBUCKET_PASSWORD}@bitbucket.org/#{BITBUCKET_USERNAME}/#{slug}"
+  local_path = "#{LOCAL_DIR}/#{slug}"
+  if File.exists?(File.expand_path("#{LOCAL_DIR}/#{slug}"))
+    puts "Updating #{slug}"
+    `cd #{local_path} && git fetch #{repo_url}`
+  else
+    puts "Cloning #{slug}"
+    `git clone -n #{repo_url} #{local_path}`
+  end
+  assert_success($?)
+end
+
 
 http = Net::HTTP.new("api.bitbucket.org", 443)
 http.use_ssl = true
@@ -26,14 +75,15 @@ end
 tree = YAML.load(resp.body)
 tree["repositories"].each do |repo_tree|
   slug = repo_tree["slug"]
-  repo_url = "https://#{BITBUCKET_USERNAME}:#{BITBUCKET_PASSWORD}@bitbucket.org/#{BITBUCKET_USERNAME}/#{slug}"
-  local_path = "#{LOCAL_DIR}/#{slug}"
-  if File.exists?(File.expand_path("#{LOCAL_DIR}/#{slug}"))
-    puts "Updating #{slug}"
-    `cd #{local_path} && hg pull #{repo_url}`
-  else
-    puts "Cloning #{slug}"
-    `hg clone -U #{repo_url} #{local_path}`
+  case repo_tree['scm']
+    when "hg"
+      puts "I will be syncing #{slug} via Hg"
+      update_or_clone_hg(slug)
+    when "git"
+      puts "I will be syncing #{slug} via Git"
+      update_or_clone_git(slug)
+    else
+      puts "Cannot sync #{slug}, unknown SCM: #{repo_tree['scm']}"
   end
 end
 
