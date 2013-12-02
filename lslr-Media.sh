59cc74230b935715fe2cd5aa3e5bdf32cfc6686b
Iulian Dogariu <code@iuliandogariu.com>
2013-12-02 18:47:05 +0200

    Music now stored directly under iTunes Media. Also added Calibre Library

--- lslr-Media.sh
+++ lslr-Media.sh
@@ -15,8 +15,9 @@ dates=`date "+%Y%m%d"`
 fname=medialist-$dates
 home=/Users/iulian
 secondaryvol=/Volumes/2tb-p2
-ls -lR $secondaryvol/iTunes\ Media/Music > /tmp/$fname
+ls -lR $home/Music/iTunes/iTunes\ Media/ > /tmp/$fname
 ls -lR $secondaryvol/Movies/ >> /tmp/$fname
+ls -lR $home/Calibre\ Library/ >> /tmp/$fname
 gzip -f /tmp/$fname
 mv -f /tmp/$fname.gz $home/Dropbox/Reference/media/
 
