7b99b06622c4e88eee819aef34fc80e7e2a29e99
Iulian Dogariu <code@iuliandogariu.com>
2013-12-04 20:34:41 +0200

    Back up Calibre metadata weekly

--- lslr-Media.sh
+++ lslr-Media.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# I do this just in case I lose both my primary HDD
+# and my Time Machine HDD at once.
 # === On Linux
 # 10 18    * * *   iulian /home/iulian/dotfiles/lslr-Media.sh
 # chmod 644 /etc/cron.d/lslRMedia
@@ -14,10 +16,23 @@
 dates=`date "+%Y%m%d"`
 fname=medialist-$dates
 home=/Users/iulian
+itunesfolder=$home/Music/iTunes
+calibrefolder="$home/Calibre Library"
 secondaryvol=/Volumes/2tb-p2
-ls -lR $home/Music/iTunes/iTunes\ Media/ > /tmp/$fname
+destination=$home/Dropbox/Reference/media
+ls -lR $itunesfolder/iTunes\ Media/ > /tmp/$fname
 ls -lR $secondaryvol/Movies/ >> /tmp/$fname
-ls -lR $home/Calibre\ Library/ >> /tmp/$fname
+ls -lR "$calibrefolder/" >> /tmp/$fname
 gzip -f /tmp/$fname
-mv -f /tmp/$fname.gz $home/Dropbox/Reference/media/
+mv -f /tmp/$fname.gz $destination/
+
+if [[ $(date +%u) == 1 ]]; then
+    # It's a Monday
+    cp $itunesfolder/iTunes\ Library.itl $destination/iTunes\ Library\ $dates.itl
+fi
+
+if [[ $(date +%u) == 3 ]]; then
+    # It's a Wednesday
+    cp "$calibrefolder/metadata.db" $destination/calibre-metadata-$dates.db
+fi
 
